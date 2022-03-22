e07a51dd1bc20664d9ab6d2428b831dd8429f476
TheresaBreitwieser <theresabreitwieser@gmail.com>
2022-03-22 09:55:25 +0100

    submission homework

--- src/main/java/application/PlayWithStrings.java
+++ src/main/java/application/PlayWithStrings.java
@@ -1,8 +1,18 @@
 package application;
 
+import java.util.Arrays;
+
 public class PlayWithStrings {
 	public static void main(String[] args) {
 		System.out.println("Hello String World");
+		
+		printIntitials("Theresa", "Breitwieser");
+		
+		boolean result = areAnagrams("parliament", "partial men");
+		System.out.println(result);
+		
+		boolean palindrome = isPalindrome("Eva can I see bees in a cave");
+		System.out.println(palindrome);
 	}
 
 	/**
@@ -14,13 +24,15 @@ public class PlayWithStrings {
 	 * @param lastname
 	 */
 	public static void printIntitials(String firstname, String lastname) {
-
+		char initialFirstname = firstname.charAt(0);
+		char initialLastname = lastname.charAt(0);
+		System.out.println(initialFirstname + " " + initialLastname);
 	}
 
 	/**
 	 * TODO: check for anagrams
 	 * 
-	 * This method should determine if two strings are anagrams or not An anagram is
+	 * This method should determine if two strings are anagrams or not. An anagram is
 	 * a word or a phrase made by transposing the letters of another word or phrase.
 	 * For example, "parliament" is an anagram of "partial men," and "software" is
 	 * an anagram of "swear oft." The program should ignore white space and
@@ -31,8 +43,40 @@ public class PlayWithStrings {
 	 * @return true if the values are anagrams, false otherwise.
 	 */
 	public static boolean areAnagrams(String value1, String value2) {
-		return false;
+		// alle Leerzeichen und punctuation aus Strings entfernen
+		// beide strings in ein Array von Character umwandeln, dann sortieren und vergleichen
+		value1 = value1.replaceAll(" ", "");
+		value1 = value1.replaceAll("\\p{Punct}","");
+		value2 = value2.replaceAll(" ", "");
+		value2 = value2.replaceAll("\\p{Punct}","");
+		char[] array1 = new char[value1.length()];
+		char[] array2 = new char[value2.length()];
+		
+		if (value1.length() != value2.length()) {
+			return false;
+		}
+		
+		for(int i=0; i<value1.length(); i++) {
+			array1[i] = value1.charAt(i);
+		}
+		
+		for(int i=0; i<value2.length(); i++) {
+			array2[i] = value2.charAt(i);
+		}
+		
+		Arrays.sort(array1);
+		Arrays.sort(array2);
+		
+		if (Arrays.equals( array1, array2)) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		
 	}
+
+	
 
 	/**
 	 * TODO: palindrone checker
@@ -49,6 +93,17 @@ public class PlayWithStrings {
 	 * @return true if it is a palindrome, false otherwise.
 	 */
 	public static boolean isPalindrome(String value) {
-		return false;
+		//String builder, String in umgekehrter Reihenfolge ausgeben und Strings vergleichen
+		StringBuilder builder = new StringBuilder();
+		value=value.replaceAll(" ", "");
+		builder.append(value);
+		builder.reverse();
+		String value2 = builder.toString();
+			
+			if(value.equalsIgnoreCase(value2)) {
+				return true;
+			} else {
+				return false;	
+			}
 	}
 }
